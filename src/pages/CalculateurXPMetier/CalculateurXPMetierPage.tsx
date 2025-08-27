05c9cbf7862097689c98312907b9664aace53250
mylgasia <94791767+GuillaumeBataille@users.noreply.github.com>
2025-08-28 00:14:58 +0200

    Test publish githubpage

--- src/pages/CalculateurXPMetier/CalculateurXPMetierPage.tsx
+++ src/pages/CalculateurXPMetier/CalculateurXPMetierPage.tsx
@@ -1,8 +1,25 @@
-import { createPage } from "../../utils/createPage";
+import React from "react";
+import Layout from "../../components/Layout";
 import { CalculateurXPMetierProvider } from "../../contexts/CalculateurXPMetierContext";
 
-const CalculateurXPMetierPage = createPage("CalculateurXPMetier", [
-  (children) => <CalculateurXPMetierProvider>{children}</CalculateurXPMetierProvider>,
-]);
+// Imports directs de tes composants
+import CalculateurXPMetierTop from "./CalculateurXPMetierTop";
+import CalculateurXPMetierLeft from "./CalculateurXPMetierLeft";
+import CalculateurXPMetierCenter from "./CalculateurXPMetierCenter";
+import CalculateurXPMetierRight from "./CalculateurXPMetierRight";
+
+const CalculateurXPMetierPage: React.FC = () => {
+  return (
+    /*<CalculateurXPMetierProvider>
+      <Layout
+        top={<CalculateurXPMetierTop />}
+        left={<CalculateurXPMetierLeft />}
+        center={<CalculateurXPMetierCenter />}
+        right={<CalculateurXPMetierRight />}
+      />
+    </CalculateurXPMetierProvider>*/
+    <div></div>
+  );
+};
 
 export default CalculateurXPMetierPage;
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,3 +1,21 @@
-import { createPage } from "../../utils/createPage";
-const HomePage = createPage("Home");
+import React from "react";
+import Layout from "../../components/Layout";
+
+// Imports directs des composants Home
+import HomeTop from "./HomeTop";
+import HomeLeft from "./HomeLeft";
+import HomeCenter from "./HomeCenter";
+import HomeRight from "./HomeRight";
+
+const HomePage: React.FC = () => {
+  return (
+    <Layout
+      top={<HomeTop />}
+      left={<HomeLeft />}
+      center={<HomeCenter />}
+      right={<HomeRight />}
+    />
+  );
+};
+
 export default HomePage;
